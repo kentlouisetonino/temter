5539a1ae5044a01cbe40480079592158da2868a6
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-07-24 22:15:05 +0800

    Implement the option input.

---
+++ src/inputs/app_option.c
@@ -0,0 +1,16 @@
+#include "../displays/ansi_color_codes.h"
+#include "../displays/escape_sequence.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int app_option(void) {
+  int option = 0;
+  char *garbage_buffer = malloc(sizeof(char) * 50);
+
+  add_new_tab();
+  printf(" %sEnter the option:%s ", BLUE, RESET);
+  scanf("%d", &option);
+  fgets(garbage_buffer, 50, stdin);
+
+  return option;
+}
--- src/main.c
+++ src/main.c
@@ -2,23 +2,38 @@
 #include "displays/app_options.h"
 #include "displays/clear_screen.h"
 #include "displays/escape_sequence.h"
+#include "inputs/app_option.h"
 #include <stdio.h>
 
 int main(void) {
-  // Clean up the screen.
-  clear_screen();
-  add_new_line();
-  add_new_line();
+  int option;
 
-  // Show the app description.
-  app_description();
-  add_new_line();
-  add_new_line();
+  while (1) {
+    // Clean up the screen.
+    clear_screen();
+    add_new_line();
+    add_new_line();
 
-  // Show the app options.
-  app_options();
-  add_new_line();
-  add_new_line();
+    // Show the app description.
+    app_description();
+    add_new_line();
+    add_new_line();
+
+    // Show the app options.
+    app_options();
+    add_new_line();
+    add_new_line();
+
+    // Ask the user for their input.
+    option = app_option();
+
+    // Reset the loop if invalid input.
+    if (option == 0) {
+      continue;
+    };
+
+    break;
+  }
 
   return 0;
 }
